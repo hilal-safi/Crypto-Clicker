45955e52bae0bd5737358a0524961a4fead04a82
Hilal Safi <hilalsafi0@gmail.com>
2024-09-09 17:30:48 -0400

    Initial Commit

---
+++ Crypto Clicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crypto Clicker
+//
+//  Created by Hilal Safi on 2024-09-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypto Clicker/Crypto_ClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_ClickerApp.swift
+//  Crypto Clicker
+//
+//  Created by Hilal Safi on 2024-09-09.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
