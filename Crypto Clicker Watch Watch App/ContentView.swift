a300fd365015142786ecbbc4c75a441898ffa429
Hilal Safi <hilalsafi0@gmail.com>
2025-01-04 14:18:56 -0500

    Started WatchOS companion app

---
+++ Crypto Clicker Watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crypto Clicker Watch Watch App
+//
+//  Created by Hilal Safi on 2025-01-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypto Clicker Watch Watch App/Crypto_Clicker_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_Clicker_WatchApp.swift
+//  Crypto Clicker Watch Watch App
+//
+//  Created by Hilal Safi on 2025-01-04.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_Clicker_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
